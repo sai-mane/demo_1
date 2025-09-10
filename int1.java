a2cc5da3c0b402e8b802651766fef5365c6282e3
sai-mane <sai1822001@gmail.com>
2025-09-10 12:11:45 +0530

    Added files

---
+++ int1.java
@@ -0,0 +1,13 @@
+ public interface int1{
+public void get();
+}
+class a implements int1{
+public void get(){
+System.out.print("world");
+}}
+public class main{
+public static void main(String[] a1){
+a b=new a();
+b.get();
+}
+}
